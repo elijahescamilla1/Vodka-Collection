9f806026401fb79ffe6c9ef651e05d273cb5f600
Elijah Escamillagit <elijah.escamilla@gmail.com>
2024-07-21 06:38:10 -0700

    More updates for files

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///vodka_collection.db'
+    SECRET_KEY = os.environ.get('Anaheim')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgres://uapja7qv2u2sg4:p00802806ca704c63ed598aab27828902898e87a27ae62c932d71d0aaab1c0711@c5hilnj7pn10vb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d33pslt2mm5jv2')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwtsecretkey'
+
+
