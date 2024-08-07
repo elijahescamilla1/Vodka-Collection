f481c068e58f24404b8157c64798d7d5ee94c4a4
elijahescamilla1 <154039956+elijahescamilla1@users.noreply.github.com>
2024-08-07 05:17:45 -0700

    Configure hosted access update

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, jsonify
+from config import Config
+from models import db
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+jwt = JWTManager(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def home():
