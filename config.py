84aa75b1bb3e126c01838ac3350ab071ee5c1323
Elijah Escamilla <elijah.escamilla@gmail.com>
2024-08-08 19:25:26 -0700

    Updates for files

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///vodka_collection.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL', 
+        'postgresql://postgres:TITOS__VODKA@aws-0-us-west-1.pooler.supabase.com:6543/postgres'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'default_jwt_secret_key')
