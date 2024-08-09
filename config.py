57ebd1a5461f07f0a9375ce04d2fa511c0042aa7
Elijah Escamilla <elijah.escamilla@gmail.com>
2024-08-09 01:05:04 -0700

    Fix database connection string

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL', 
-        'postgresql://postgres:TITOS__VODKA@aws-0-us-west-1.pooler.supabase.com:6543/postgres'
+        'postgresql://postgres.clhjwoarzpcuhrnkdryh:[LLH4YvWLYjp5VIsA]@aws-0-us-west-1.pooler.supabase.com:6543/postgres'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'default_jwt_secret_key')
