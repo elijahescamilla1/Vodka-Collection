842b8f7206640680396abb149c6ed74314b105bf
Elijah Escamilla <elijah.escamilla@gmail.com>
2024-08-12 06:35:39 -0700

    Updates for firebase

--- config.py
+++ config.py
@@ -2,9 +2,13 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
+    
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL', 
-        'postgresql://postgres.clhjwoarzpcuhrnkdryh:[LLH4YvWLYjp5VIsA]@aws-0-us-west-1.pooler.supabase.com:6543/postgres'
+        'postgresql://postgres.clhjwoarzpcuhrnkdryh:[VODKACOLLECTION125]@aws-0-us-west-1.pooler.supabase.com:6543/postgres'
     )
+    
+    # Disable modification tracking as it is not needed
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'default_jwt_secret_key')
